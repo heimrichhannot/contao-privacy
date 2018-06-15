52f7b167fe0e7dfda6013d0dd7448a9abc69b36f
Dennis Patzer <d.patzer@heimrich-hannot.de>
2018-06-15 15:00:32 +0200

    see cl 1.1.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,6 +1,12 @@
 # Changelog
 All notable changes to this project will be documented in this file.
 
+## [1.1.1] - 2018-06-15
+
+### Changed
+
+- localization
+
 ## [1.1.0] - 2018-06-08
 
 ### Added
--- languages/de/default.php
+++ languages/de/default.php
@@ -3,7 +3,7 @@
 $lang = &$GLOBALS['TL_LANG']['MSC']['huhPrivacy'];
 
 $lang['addPrivacyProtocolEntry'][0]         = 'Eintrag im Datenschutzprotokoll hinzufügen';
-$lang['addPrivacyProtocolEntry'][1]         = 'Wählen Sie diese Option, um beim Abschicken des Formulars einen Eintrag im Datenschutzprotokoll hinzuzufügen.';
+$lang['addPrivacyProtocolEntry'][1]         = 'Wählen Sie diese Option, um nach der Interaktion einen Eintrag im Datenschutzprotokoll hinzuzufügen.';
 $lang['privacyProtocolEntryArchive'][0]     = 'Archiv';
 $lang['privacyProtocolEntryArchive'][1]     = 'Wählen Sie hier das Archiv aus, in dem der Eintrag im Datenschutzprotokoll gespeichert werden soll.';
 $lang['privacyProtocolEntryType'][0]        = 'Typ';
